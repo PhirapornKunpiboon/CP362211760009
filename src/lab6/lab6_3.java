15e9840a07ea21d3edeff72debed4cf2014922c8
phirapornkp@nvc.ac.th <Phirapornkp@nvc.ac.th>
2019-08-22 16:02:51 +0700

    lab6

---
+++ src/lab6/lab6_3.java
@@ -0,0 +1,23 @@
+package lab6;
+
+import com.sun.xml.internal.ws.wsdl.writer.document.soap.Body;
+
+import java.util.Scanner;
+
+public class lab6_3 {
+    public static void main(String[] args) {
+        {
+            //BMI = weight/(hight*hight)
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("weight: ");
+            float weight = in.nextFloat();
+
+            System.out.println("hight: ");
+            float hight = in.nextFloat();
+
+            float bmi = weight/(hight*hight);
+            System.out.println(bmi);
+        }
+    }
+}
